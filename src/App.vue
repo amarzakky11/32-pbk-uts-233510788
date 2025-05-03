9644d90c26644f64ecac77bd4fa5352c78807c99
amarzakky11 <muhammadamarzakky@student.uir.ac.id>
2025-05-03 14:03:47 +0700

    filteredTasks

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const tasks = ref([])
 const newTask = ref('')
+const filtered = ref('all')
 
 const addTask = () => {
   if (newTask.value !== '') {
@@ -24,15 +25,30 @@ const toggleTask = (task) => {
   console.log(task.completed)
 }
 
+const filteredTasks = computed(() => {
+  if (filtered.value === 'all') {
+    return tasks.value
+  } else if (filtered.value === 'completed') {
+    return tasks.value.filter(task => task.completed)
+  } else {
+    return tasks.value.filter(task => !task.completed)
+  }
+})
+
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task">
     <button @click="addTask">Add Task</button>
+    <select v-model="filtered">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="incomplete">Incomplete</option>
+    </select>
 
     <ul>
-      <li v-for="task in tasks" :key="task.id">
+      <li v-for="task in filteredTasks" :key="task.id">
         <input type="checkbox" @change="toggleTask(task)" v-model="task.completed">
         {{ task.text }}
         <button @click="removeTask(task)">Remove</button>
