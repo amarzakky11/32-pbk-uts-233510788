bfd81c4171ef37e6687753b07648c6245f9bf56d
amarzakky11 <muhammadamarzakky@student.uir.ac.id>
2025-05-03 13:56:08 +0700

    addTask and display

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    text: newTask.value,
+    completed: false
+  })
+  newTask.value = ''
+}
+
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task">
+    <button @click="addTask">Add Task</button>
+
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.text }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
